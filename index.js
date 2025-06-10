284ecd8d733c93a6b3484905be772101d8ba70f3
AngelSony <angelfol@outlook.com>
2025-06-09 21:22:01 -0300

    Index del programa

---
+++ index.js
@@ -0,0 +1,28 @@
+let palabras;
+
+function main(){
+  incializar();
+  palabra = elegirPalbra();
+  mostrarPalabraOculta(palabra);
+}
+
+async function inicializar(){
+  //Cargar banco de palabras
+  try {
+    const response = await fetch('palabras.json');
+    datos = await response.json();
+    consele.log(palabras);
+  } catch (error) {
+    console.error('Error al cargar el JSON:', error);
+  }
+
+  //Cargar Dificultades
+}
+
+function elegirPalabra(){
+  return;
+}
+
+function mostrarPalabraOculta(palabra) {
+  return "_";
+}
