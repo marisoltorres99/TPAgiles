ec3b0d6aaab77f22a428fff19abed64c2f961958
germanpachec0 <germanpacheco98@gmail.com>
2025-06-30 20:46:39 -0300

    agregado de funcion gano, perdio y mostrar estado

--- ahorcado.js
+++ ahorcado.js
@@ -1,24 +1,64 @@
-class Ahorcado{ 
+class Ahorcado {
   constructor(palabra) {
     this.palabra = palabra;
     this.letrasAdivinadas = [];
     this.letrasFallidas = [];
-    this.fallosRestantes = 6; 
+    this.fallosRestantes = 6;
     this.dificultad = "";
     this.arrayPalabras = this.palabra.split("");
   }
   adivinarLetra(letraAdivinada) {
-  //Checkear si la letra está en alguno de los arreglos de letras adivinadas o fallidas
-  if (this.palabra.includes(letraAdivinada)){
-    //Agregar la letra en el array letrasAdivinadas
+    if (this.letrasAdivinadas.includes(letraAdivinada) || this.letrasFallidas.includes(letraAdivinada)) {
+      console.log(`"La letra "${letraAdivinada} ya fue intentada"`);
+      return;
+    }
+    if (this.palabra.includes(letraAdivinada)) {
+      this.letrasAdivinadas.push(letraAdivinada);
+    } else {
+      this.letrasFallidas.push(letraAdivinada);
+      this.fallosRestantes -= 1;
+    }
+
+    //Mostrar Palabra oculta e intentos restantes
+
+
+
+    if (this.gano()) {
+      console.log("Ganaste la palabra es", this.palabra)
+      return
+    }
+    if (this.perdio()) {
+      console.log("Perdio la partida la palabra era: ", this.palabra)
+    }
+
+    return false;
+  }
+
+  mostrarPalabraOculta() {
+      let palabraAMostrar = ""
+      for (const letra of this.palabra) {
+      if (this.letrasAdivinadas.includes(letra)) {
+        palabraAMostrar += letra
+      } else {
+        palabraAMostrar += "_"
+      }
+      }
+    return palabraAMostrar.join(" ");
+  }
+
+  gano() {
+    for (const letra of this.arrayPalabras) {
+      if (!this.letrasAdivinadas.includes(letra)) {
+        return false;
+      }
+    }
     return true;
   }
-    //Agregar la letra en el array letrasFallidas
-    //Restar un intento
-    return false;
-  //Checkear si ganó o no
-  //Mostrar Palabra oculta e intentos restantes
-}
-} 
 
-module.exports= Ahorcado;
+  perdio() {
+    return this.fallosRestantes === 0;
+  }
+
+}
+
+module.exports = Ahorcado;
